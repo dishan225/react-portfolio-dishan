0d927121dd2bd5ac0174770cdf2ce3e566409fa6
DISHAN <152848633+dishan225@users.noreply.github.com>
2025-01-02 13:42:06 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,19 @@ const Footer = () => {
       <footer className="text-center py-4 bg-neutral-900 text-neutral-400 text-sm">
         <p>© {new Date().getFullYear()} Dishan Madhuranga. All rights reserved.</p>
         <p>
-  Designed and developed with precision by <a href="/" className="hover:text-purple-400">Dishan Maduranga</a>
-</p>
-
+          Designed and developed with precision by 
+          <a 
+            href="/" 
+            className="hover:text-purple-400"
+            aria-label="Visit Dishan Madhuranga's homepage"
+          >
+            Dishan Madhuranga
+          </a>
+        </p>
       </footer>
     );
 };
   
 export default Footer;
+
 
