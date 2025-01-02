41424302156f9a64f5e53d5dc7de8ece7cfb817d
DISHAN <152848633+dishan225@users.noreply.github.com>
2025-01-02 16:33:45 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,18 @@
 import { PROJECTS } from "../constants";
 import { motion } from "framer-motion";
+import { FaGithub } from "react-icons/fa";
 
 const Project = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4">
-      <motion.h1 
-      whileInView={{opacity:1, y:0}}
-      initial={{opacity:1, y:-100}}
-      transition={{duration:0.5}}
-      className="my-20 text-center text-4xl">Projects</motion.h1>
+    <div id="projects" className="border-b border-neutral-900 pb-4">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 1, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h1>
       <div>
         {PROJECTS.map((project, index) => (
           <div
@@ -16,11 +20,12 @@ const Project = () => {
             className="mb-12 flex flex-col items-center lg:flex-row lg:items-start lg:justify-center gap-6"
           >
             {/* Image Section */}
-         <motion.div 
-          whileInView={{opacity:1, x:0}}
-          initial={{opacity:1, x:-100}}
-          transition={{duration: 0.5}}
-          className="w-full lg:w-1/4 flex ">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 1, x: -100 }}
+              transition={{ duration: 0.5 }}
+              className="w-full lg:w-1/4 flex"
+            >
               <img
                 src={project.image}
                 width={150}
@@ -31,15 +36,15 @@ const Project = () => {
             </motion.div>
 
             {/* Text Section */}
-          <motion.div 
-            
-            whileInView={{opacity:1, x:0}}
-            initial={{opacity:1, x:100}}
-            transition={{duration: 0.5}}
-            className="w-full max-w-sm lg:w-3/4">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 1, x: 100 }}
+              transition={{ duration: 0.5 }}
+              className="w-full max-w-sm lg:w-3/4"
+            >
               <h6 className="text-xl font-semibold mb-2">{project.title}</h6>
               <p className="mb-4 text-neutral-400">{project.description}</p>
-              <div className="flex flex-wrap gap-2">
+              <div className="flex flex-wrap gap-2 mb-4">
                 {project.technologies.map((tech, index) => (
                   <span
                     key={index}
@@ -49,7 +54,7 @@ const Project = () => {
                   </span>
                 ))}
               </div>
-            {project.githubLink && (
+              {project.githubLink && (
                 <a
                   href={project.githubLink}
                   target="_blank"
